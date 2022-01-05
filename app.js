ae62c9eca082497a5b4752b332aad14f29d14e44
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-05 09:05:34 -0700

    added dotenv

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var path = require('path');
+var https = require('https');
 var express = require('express');
+var url = require('url');
+// allows access to process.env
+require('dotenv').config();
 
 var app = express();
 
@@ -10,6 +14,29 @@ app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get(/^\/(\d{5})$/, function (req, res, next) {
+  let ZIPCODE = req.url.slice(1);
+  const options = {
+    hostname: 'api.openweathermap.org',
+    path: `data/2.5/weather?zip=${ZIPCODE}&appid=${process.env.API_KEY}`,
+    method: 'GET',
+  };
+
+  const REQUEST = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    REQUEST.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  REQUEST.on('error', (error) => {
+    console.error(error);
+  });
+
+  res.end();
 });
 
 app.use(function (req, res) {
